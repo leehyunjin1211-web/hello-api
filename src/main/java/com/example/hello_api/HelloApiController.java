dc2070bb801d5d522816047a5e01b5bddbe19d71
leehyunjin <leehyunjin1211@gmail.com>
2025-10-01 16:50:51 +0900

    First commit for hello-api project

---
+++ src/main/java/com/example/hello_api/HelloApiController.java
@@ -0,0 +1,21 @@
+package com.example.hello_api;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@RestController
+public class HelloApiController {
+
+        @GetMapping("/hello")
+        public Map<String, Object> sayHello(){
+
+                return Map.of(
+                                "message", "Hello, World!",
+                                "timesptamp", System.currentTimeMillis(),
+                                "koreatime", ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toString()
+                             );
+        }
+}  
